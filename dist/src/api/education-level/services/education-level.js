ce42e2818add3a0073155e9651de1edf62c22210
Mouhealdien <81492994+Mouhealdien@users.noreply.github.com>
2023-08-18 18:15:05 +0300

    Add files via upload

---
+++ dist/src/api/education-level/services/education-level.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * education-level service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::education-level.education-level');
---
+++ dist/src/api/job-role/controllers/job-role.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * job-role controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::job-role.job-role');
